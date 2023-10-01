378e36cbbb44107c55a1c301d4f14d44a320ef06
EricBC04 <91167431+EricBC04@users.noreply.github.com>
2023-09-30 22:59:43 -0300

    Teste-vercel

--- api/routes/produtos.js
+++ api/routes/produtos.js
@@ -7,7 +7,53 @@ const {db, ObjectId} = await connectToDatabase()
 const nomeCollection = 'informatica'
 
 const validaProduto = [
-    
+     check('produto')
+            .not()
+            .isEmpty()
+            .trim()
+            .withMessage("É obrigatório informar o produto")
+            .isAlphanumeric('pt-BR', {ignore: '/. '}),
+    check('marca')
+            .not()
+            .isEmpty()
+            .trim()
+            .withMessage("É obrigatório informar qual a marca"),
+    check('preco')
+            .not()
+            .isEmpty()
+            .trim()
+            .withMessage("É obrigatório informar o preco")
+            .isNumeric()
+            .withMessage('O preço só deve conter números'),
+    check('estoque')
+            .not()
+            .isEmpty()
+            .trim()
+            .withMessage("É obrigatório informar a quantidade no estoque")
+            .isNumeric()
+            .withMessage('A quantidade informada só deve conter números'),
+    check('cor')
+            .not()
+            .isEmpty()
+            .trim()
+            .withMessage("É obrigatório informar qual a cor do produto"),
+    check('data_lancamento')
+            .not()
+            .isEmpty()
+            .trim()
+            .withMessage('É obrigatório informar a Data de nascimento'),
+    check('classificacao')
+            .not()
+            .isEmpty()
+            .trim()
+            .withMessage("É obrigatório informar qual a classificação do produto"),
+    check('peso')
+            .not()
+            .isEmpty()
+            .trim()
+            .withMessage("É obrigatório informar o peso do produtp")
+            .isNumeric()
+            .withMessage('O peso só deve conter números'),
 ]
 
 /*
@@ -49,6 +95,61 @@ router.get('/id/:id', async(req, res) => {
         res.status(500).json({"error": err.message})
     }
 })
+
+/**
+ * GET /api/produtos/produto/:produto
+ * Lista os produtos de serviço pelo nome 
+ */
+router.get('/produto/:produto', async(req, res)=> {
+    try{
+        db.collection(nomeCollection)
+        .find({'produto': {$regex: req.params.nome, $options: "i"}})
+        .toArray((err, docs) => {
+            if(err){
+                res.status(400).json(err) // bad request
+            } else {
+                res.status(200).json(docs) // retorna o documento
+            }
+        })
+    } catch (err) {
+        res.status(500).json({"error": err.message})
+    }
+})
+
+/**
+ * GET /api/produtos/marca/:marca
+ * Lista os produtos pela marca 
+ */
+router.get('/marca/:marca', async(req, res)=> {
+    try{
+        db.collection(nomeCollection)
+        .find({'cpf': {$eq: req.params.cpf}})
+        .toArray((err, docs) => {
+            if(err){
+                res.status(400).json(err) // bad request
+            } else {
+                res.status(200).json(docs) 
+            }
+        })
+    } catch (err) {
+        res.status(500).json({"error": err.message})
+    }
+})
+
+router.get("/preco/:preco", async (req, res) => {
+	try {
+		db.collection(nomeCollection)
+			.find({
+				preco: { $lte: req.params.preco },
+			})
+			.toArray((err, docs) => {
+				if (err) res.status(400).json(err); // bad request
+				else res.status(200).json(docs);
+			});
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
 
 /*
 * POST /api/produtos
